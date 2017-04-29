2858001341920f15af0de04638b012bc14dd3c82
Mitchell Mohorovich <mitchell.mohorovich@gmail.com>
2017-04-29 16:06:47 -0400

    Added californium library.

--- src/main/java/com/mohorovich/mitchell/node/CoAPNode.java
+++ src/main/java/com/mohorovich/mitchell/node/CoAPNode.java
@@ -1,5 +1,7 @@
 package com.mohorovich.mitchell.node;
 
+import org.eclipse.californium.core.coap.CoAP;
+
 /**
  * This node is a simple dummy CoAP node that responds to every request with the same payload data.
  */
